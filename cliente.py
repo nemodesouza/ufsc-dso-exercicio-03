c4aac45f85c609c442766f3703ddd453eabbde79
NEMO DE SOUZA <53828046+nemodesouza@users.noreply.github.com>
2021-04-15 16:41:00 -0300

    Arquivos necessários e implementação da classe Cliente

---
+++ cliente.py
@@ -0,0 +1,23 @@
+
+
+class Cliente:
+
+    def __init__(self):
+        self.__nome = nome
+        self.__fone = fone
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
+
+    @property
+    def fone:
+        return self.__fone
+
+    @fone.setter
+        self.__fone = fone
+
